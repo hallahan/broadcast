1217e387dee59064cc768d4e24c07d75f0d6a65a
Nicholas Hallahan <nick@theoutpost.io>
2012-05-21 10:57:04 -0700

    fixed file names and got rid of stuff deleted

--- builder.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-echo "Turning on coffeescript compiler in watch mode..."
-./node_modules/.bin/browserify src/client.coffee --watch -o public/client.js & ./node_modules/coffee-script/bin/coffee -cw -o lib/ src/*.coffee
--- src/client.coffee
+++
@@ -1,6 +0,0 @@
-# The Outpost Broadcast for The Smallest Federated Wiki
-# by Nicholas Hallahan
-# http://broadcast.theoutpost.io
-
-# Used by browserify to create a single js for the client.
-require('./controller.coffee')
